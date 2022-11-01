cb4d952852678048082bb94bd1945724c675fa5e
osawa-koki <osawa-koki@osawa.dev>
2022-11-01 21:39:42 +0900

    HTTP動詞の基本的なマッピング。

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,15 +3,31 @@ package com.example.demo;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoApplication {
 
-  @GetMapping("/")
-  public String pets() {
-    return "Hello World";
+  @GetMapping("/hello")
+  public String hello_get() {
+    return "Hello Get!!!";
   }
+  @PostMapping("/hello")
+  public String hello_post() {
+    return "Hello Post!!!";
+  }
+  @PutMapping("/hello")
+  public String hello_put() {
+    return "Hello Put!!!";
+  }
+  @DeleteMapping("/hello")
+  public String hello_delete() {
+    return "Hello Delete!!!";
+  }
+
 
 }
