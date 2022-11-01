8a519038072eefe1f58c48b6bd9f134d2bca5582
osawa-koki <osawa-koki@osawa.dev>
2022-11-01 21:32:35 +0900

    基本部分完成した。

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.concurrent.atomic.AtomicLong;
 
-@SpringBootApplication
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+  @GetMapping("/")
+  public String pets() {
+    return "Hello World";
+  }
 
 }
